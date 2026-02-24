c428db25f814a576b94fb1710f5a4c7835092e09
x <x>
2026-02-24 10:22:53 -0500

    multiplicacion

---
+++ mult.py
@@ -0,0 +1,8 @@
+print("Ingresando")
+print("")
+numero1= int(input("Ingrese primer digito: "))
+numero2= int(input("Ingrese segundo digito: "))
+
+mult= numero1*numero2
+
+print(f"El resultado es: {mult}")

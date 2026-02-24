be58be288e70f364a10123a75493cf00ed386575
x <x>
2026-02-24 10:18:25 -0500

    se agrego una suma

---
+++ suma.py
@@ -0,0 +1,7 @@
+print("text")
+numero_1= int(input("Digita primer numero: "))
+numero_2= int(input("Digita segundo numero: "))
+
+suma = numero_1 + numero_2
+
+print(f"el resultado de la suma es: {suma}")
